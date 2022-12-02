d4e5bddc41c06f0dc9056a0381feceb040211284
dav1taa <nebieridze.dato12@gmail.com>
2022-12-02 23:53:14 +0400

    საბოლოო <3

--- project/index.js
+++ project/index.js
@@ -1,7 +1,7 @@
 
-// setTimeout(function(){
-//         window.location.href = 'allmovies.html';
-//     }, 5000);
+setTimeout(function(){
+        window.location.href = 'allmovies.html';
+    }, 5000);
 
 
 
